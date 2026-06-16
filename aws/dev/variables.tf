466bd386297da4d40ead35183c9b9e3422fc73d7
Lejiend <lejiend@Lejiends-MacBook-Pro.local>
2026-06-16 16:19:07 +0800

    feature: add rds mysql in the private subnet and allow to be connected from ec2

--- aws/dev/variables.tf
+++ aws/dev/variables.tf
@@ -51,3 +51,27 @@ variable "vpc_cidr" {
   type        = string
   default     = "10.0.0.0/16"
 }
+
+variable "db_instance_class" {
+  description = "RDS instance class"
+  type        = string
+  default     = "db.t4g.micro"
+}
+
+variable "db_name" {
+  description = "Database name"
+  type        = string
+  default     = "locker"
+}
+
+variable "db_username" {
+  description = "Database master username"
+  type        = string
+  default     = "admin"
+}
+
+variable "db_password" {
+  description = "Database master password"
+  type        = string
+  sensitive   = true
+}
